6aaa63da0963e455428bb09220708aca80babe98
Alexandra Mateva <alex.mateva@gmail.com>
2016-03-24 14:28:12 +0200

    add week9 and week10

--- week2/test.py
+++
@@ -1,9 +0,0 @@
-def main():
-    filename = 'panda.txt'
-    data = open(filename, 'w')
-#    with open(fileneme, 'w') as data:
-    data.write("Hello, I'm Panda\n")
-    data.close()
-
-if __name__ == '__main__':
-    main()
